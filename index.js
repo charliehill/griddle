c70ad664e1206be35b0c3b07e854803c0fdbe212
Charlie Hill <charliehill@gmail.com>
2021-12-09 20:31:42 -0600

    Implemented server-based game state

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+// Game state
+const boardSize = 3;
+var game = [boardSize];
+for (let i=0; i<boardSize; i++) {
+    game[i] = new Array(boardSize); 
+    for (let j=0; j<boardSize; j++) {
+        game[i][j] = {name:"", color:""}; // Initializing every cell to no user, default color
+    }
+}
+console.log(game); 
+
 // Define client routes
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -13,13 +24,13 @@ app.get('/', (req, res) => {
 app.get('/grid', (req, res) => {
     res.sendFile(__dirname + '/mingrid.html');
 });
-  
+
 // Listen for events
 io.on('connection', (socket) => {
     // Connect/disconnect
     console.log('User connected');
     socket.on('disconnect', () => {
-      console.log('user disconnected');
+      console.log('User disconnected');
     });
     // Chat 
     socket.on('Chat message', (msg) => {
@@ -28,7 +39,8 @@ io.on('connection', (socket) => {
     });
     // Grid
     socket.on('cell click', (clickinfo) => {
-        console.log("User " + clickinfo.user + " clicked cell " + clickinfo.cell);
+        console.log("Name " + clickinfo.name + ", color " + clickinfo.color + ", clicked cell " + clickinfo.cell);
+        updateGame(clickinfo);
         io.emit('cell click', clickinfo);
     });
 });
@@ -37,3 +49,17 @@ io.on('connection', (socket) => {
 server.listen(3000, () => {
   console.log('Listening on *:3000');
 });
+
+// updateGame() returns true if board should be updated
+function updateGame(clickinfo) { 
+    // Extract the cell number
+    const cellInfo = clickinfo.cell.split("."); // cell ID is in format "cell.XX.YY"
+    let cellX = cellInfo[1];
+    let cellY = cellInfo[2];
+    console.log("X: " + cellX + " Y: " + cellY); 
+
+    // Last click gets the cell
+    game[cellX][cellY].name = clickinfo.name; 
+    game[cellX][cellY].color = clickinfo.color; 
+    console.log(game[cellX][cellY]); 
+}
